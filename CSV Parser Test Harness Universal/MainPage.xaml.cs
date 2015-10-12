ea604cd82624009d5867bd1c37aed86902cc2cd6
Frank La Vigne <frankchevy@hotmail.com>
2015-10-11 23:53:24 -0400

    Added some UX to the test harness app. TODOs: 1. use XAML data binding instread of generating UI via code 2. find a more cheerful data set :)

--- CSV Parser Test Harness Universal/MainPage.xaml.cs
+++ CSV Parser Test Harness Universal/MainPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Navigation;
 using CSVParserWinRT;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -28,26 +29,76 @@ namespace CSV_Parser_Test_Harness_Universal
     {
         private const string SAMPLE_CSV_DATA_URL = "https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.csv?accessType=DOWNLOAD";
 
+        public IEnumerable<Dictionary<string, string>> Data { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
 
-            ParseData();
+            DownloadAndParseData();
+        }
+
+        private void BuildPivotContent()
+        {
+            // TODO: use DataBinding instead of generating controls by code
+
+            var uniqueYears =
+                (from data in this.Data
+                 select data["Year"]).Distinct().OrderBy(x => x).ToList();
+            
+            uniqueYears.ForEach(x => {
+
+                var pivotItem = new PivotItem() { Header = x };
+                var grid = new Grid();
+
+                var dataForYear =
+                (from data in this.Data
+                 select data)
+                 .Where(y => y["Year"] == x)
+                 .Distinct()
+                 .OrderBy(z => z["Count"]);
+
+                StringBuilder causes = new StringBuilder();
+
+            dataForYear.ToList()
+            .ForEach(line =>
+                    {
+                        var lineString = string.Format("{0} ({1}, {2})", line["Cause of Death"], line["Sex"], line["Ethnicity"]);
+                        causes.AppendLine(lineString);
+                    }
+                );
+
+                var textBlock = new TextBlock()
+                    {
+                        Text = causes.ToString()
+                    };
+
+                ScrollViewer sv = new ScrollViewer();
+                sv.Content = textBlock;
+
+                grid.Children.Add(sv);
+
+                pivotItem.Content = grid;
+                pvtPivot.Items.Add(pivotItem);
+
+
+            } );
+
+
+
 
         }
 
-        private async void ParseData()
+        private async void DownloadAndParseData()
         {
             CsvParser csvParser = new CsvParser();
             var rawText = await DownloadCsvData();
             csvParser.RawText = rawText;
 
-            var parsedResults = await csvParser.Parse();
+            this.Data = await csvParser.Parse() as IEnumerable<Dictionary<string, string>>;
 
-            var queryAgainstResults = parsedResults
-                .Where(x => x["Year"] == "2010" && x["Sex"] == "MALE")
-                .ToList();
-                ;
+            BuildPivotContent();
+
 
         }
 
